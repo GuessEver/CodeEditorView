745aa13e03fc77df89a733f3283eb26bcb14d7c9
GuessEver <guessever@gmail.com>
2016-08-21 09:49:29 +0800

    Initial Commit

---
+++ CodeEditorView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodeEditorView
+//
+//  Created by Sunnycool on 16/8/21.
+//  Copyright © 2016年 GuessEver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CodeEditorView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CodeEditorView
+//
+//  Created by Sunnycool on 16/8/21.
+//  Copyright © 2016年 GuessEver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
